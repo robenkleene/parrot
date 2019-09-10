36a40c7815a8d6cacc992e1654e1952daad16e89
Roben Kleene <contact@robenkleene.com>
2019-09-10 18:27:44 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+force=false
+while getopts ":fh" option; do
+  case "$option" in
+    f)
+      force=true
+      ;;
+    h)
+      echo "Usage: ./install.sh [-hf]"
+      exit 0
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument" >&2
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+cd "$(dirname "$0")"
+
+dir=~/Music/Ableton/User\ Library/Presets/Instruments/Max\ Instrument/
+if [[ ! -d "$dir" ]]; then
+  echo "Directory doesn't exist $dir" >&2
+  exit 1
+fi
+
+declare -a filenames=("Parrot.amxd" "ParrotNote.amxd")
+
+for filename in "${filenames[@]}"; do
+  file=max-for-live/instruments/$filename
+  if [[ ! -f "$file" ]]; then
+    echo "File doesn't exist $file" >&2
+    exit 1
+  fi
+
+  destination=${dir}${filename}
+  if [[ -f "$destination" && "$force" != "true" ]]; then
+    echo "A file already exists at $destination use -f to override" >&2
+    exit 1
+  fi
+
+  cp "$file" "$destination"
+done
